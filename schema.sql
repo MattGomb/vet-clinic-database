e64499f870e28b259fda342ce942fc0522f41dfc
Matt Gombos <gombosonline@gmail.com>
2022-12-12 18:43:05 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,9 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-postgres=# CREATE TABLE animals(
-postgres(# ID INT NOT NULL,
-postgres(# NAME TEXT,
-postgres(# DATE_OF_BIRTH DATE,
-postgres(# ESCAPE_ATTEMPTS INT,
-postgres(# NEUTERED BOOLEAN,
-postgres(# WEIGTH_KG DECIMAL);
+CREATE TABLE animals(
+  ID INT NOT NULL,
+  NAME TEXT,
+  DATE_OF_BIRTH DATE,
+  ESCAPE_ATTEMPTS INT,
+  NEUTERED BOOLEAN,
+  WEIGTH_KG DECIMAL
+);
