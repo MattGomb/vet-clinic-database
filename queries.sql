0f7224461d3d366e2c89f4be887fed2560ad4ce3
Matt Gombos <gombosonline@gmail.com>
2022-12-13 12:50:15 -0500

    Update queries.sql as per required (first change)

--- queries.sql
+++ queries.sql
@@ -10,3 +10,38 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 and weight_kg <= 17.3;
+
+/* update table day 2 */
+
+/* setting species to unspecified then roll it back, check all states */
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+/* update species as per instructions, commit changes, check the result */
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name like '%mon';
+SELECT * FROM animals;
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+
+/* deleting all records, then rolling it back */
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+/* delete animals by criteria, create savepoint, rollback, update weight, commit */
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT point1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO point1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+SELECT * FROM animals;
+COMMIT;
