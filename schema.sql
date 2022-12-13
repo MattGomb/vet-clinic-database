4417c5bf16578e8d66186043deec7914cc67d362
Matt Gombos <gombosonline@gmail.com>
2022-12-13 11:55:13 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,13 @@
 /*create table day 1 */
 
+/*update table with species day 2 */
+
 CREATE TABLE animals(
   id INT NOT NULL,
   name TEXT,
   date_of_birth DATE,
   escape_attempts INT,
   neutered BOOLEAN,
-  weight_kg DECIMAL
+  weight_kg DECIMAL,
+  species TEXT
 );
