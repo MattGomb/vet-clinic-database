334095c75f6c5bcace9a7856cd3fec24a8e54fe6
Matt Gombos <gombosonline@gmail.com>
2022-12-14 13:00:48 -0500

    Update schema with day 3 exercise commands

--- schema.sql
+++ schema.sql
@@ -12,3 +12,25 @@ CREATE TABLE animals(
   
   species TEXT
 );
+
+/* preparing multiple tables day 3 */
+
+CREATE TABLE owners(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(255) NOT NULL,
+  age INT,
+  PRIMARY KEY (id)
+  );
+  
+CREATE TABLE species(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(255),
+  PRIMARY KEY (id)
+  );
+
+ALTER TABLE animals ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (restart with 1);
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD owner_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE animals ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
