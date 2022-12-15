7b35c71f6504882ff1cd87517764282581e18d71
Matt Gombos <gombosonline@gmail.com>
2022-12-15 11:45:10 -0500

    Add join-tables in schema.sql

--- schema.sql
+++ schema.sql
@@ -34,3 +34,30 @@ ALTER TABLE animals ADD species_id INT;
 ALTER TABLE animals ADD owner_id INT;
 ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id);
 ALTER TABLE animals ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+/* add join tables day 4 */
+
+CREATE TABLE vets(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(255),
+  age INT,
+  date_of_graduation DATE,
+  PRIMARY KEY(id)
+  );
+
+CREATE TABLE specializations(
+  species_id INT,
+  vets_id INT,
+  PRIMARY KEY(species_id, vets_id),
+  CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id),
+  CONSTRAINT fk_vets FOREIGN KEY (vets_id) REFERENCES vets(id)
+  );
+  
+CREATE TABLE visits(
+  animal_id INT,
+  vets_id INT
+  date_of_visit DATE,
+  PRIMARY KEY(animal_id, vets_id),
+  CONSTRAINT fk_animal FOREIGN KEY (animal_id) REFERENCES animals(id),
+  CONSTRAINT fk_vets FOREIGN KEY (vets_id) REFERENCES vets(id)
+  );
